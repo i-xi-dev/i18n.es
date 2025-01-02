d121698764fa5deb7eaf5003d273a9aed3a3420a
i-xi-dev <naoyuki.isono@gmail.com>
2025-01-02 23:09:37 +0900

    2.1.0

--- src/utils.ts
+++ src/utils.ts
@@ -3,11 +3,36 @@ import { Basics } from "@i-xi-dev/type";
 
 const { EMPTY: EMPTY_STRING, is: isString } = Basics.StringType;
 
+const _FALLBACK = "en";
+
+let _languageNameDictionary: Intl.DisplayNames | null = null;
+
+// `nameLocale`省略時は`language`で良いのでは → getScriptName,getRegionNameと整合性が取れないのでやめる
+export function getLanguageName(
+  language: language,
+  nameLocale: Intl.UnicodeBCP47LocaleIdentifier | Intl.Locale = _FALLBACK,
+): string {
+  const reuse = _languageNameDictionary &&
+    _languageNameDictionary.resolvedOptions().locale !==
+      (isString(nameLocale) ? nameLocale : nameLocale.baseName);
+
+  if (reuse !== true) {
+    _languageNameDictionary = new Intl.DisplayNames(nameLocale, {
+      fallback: "none",
+      //XXX languageDisplay,
+      //XXX style,
+      type: "language",
+    });
+  }
+
+  return _languageNameDictionary?.of(language) ?? EMPTY_STRING;
+}
+
 let _scriptNameDictionary: Intl.DisplayNames | null = null;
 
 export function getScriptName(
   script: script,
-  nameLocale: Intl.UnicodeBCP47LocaleIdentifier | Intl.Locale = "en",
+  nameLocale: Intl.UnicodeBCP47LocaleIdentifier | Intl.Locale = _FALLBACK,
 ): string {
   const reuse = _scriptNameDictionary &&
     _scriptNameDictionary.resolvedOptions().locale !==
@@ -28,7 +53,7 @@ let _regionNameDictionary: Intl.DisplayNames | null = null;
 
 export function getRegionName(
   region: region,
-  nameLocale: Intl.UnicodeBCP47LocaleIdentifier | Intl.Locale = "en",
+  nameLocale: Intl.UnicodeBCP47LocaleIdentifier | Intl.Locale = _FALLBACK,
 ): string {
   const reuse = _regionNameDictionary &&
     _regionNameDictionary.resolvedOptions().locale !==
@@ -43,26 +68,4 @@ export function getRegionName(
   }
 
   return _regionNameDictionary?.of(region) ?? EMPTY_STRING;
-}
-
-let _languageNameDictionary: Intl.DisplayNames | null = null;
-
-export function getLanguageName(
-  language: language,
-  nameLocale: Intl.UnicodeBCP47LocaleIdentifier | Intl.Locale = "en",
-): string {
-  const reuse = _languageNameDictionary &&
-    _languageNameDictionary.resolvedOptions().locale !==
-      (isString(nameLocale) ? nameLocale : nameLocale.baseName);
-
-  if (reuse !== true) {
-    _languageNameDictionary = new Intl.DisplayNames(nameLocale, {
-      fallback: "none",
-      //XXX languageDisplay,
-      //XXX style,
-      type: "language",
-    });
-  }
-
-  return _languageNameDictionary?.of(language) ?? EMPTY_STRING;
 }
